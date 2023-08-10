6109eb487f11ca577a459698e0ac46ec28882e2f
TobyM <tobymarkham@gmail.com>
2023-08-10 16:57:17 -0400

    This has question 1 done

--- prework.py
+++ prework.py
@@ -1,6 +1,7 @@
 #Question 1
-#adklfldafjkdf
+#Write a function to print "hello_USERNAME!" is the input of the function.  The first 
+#line of the code has been defined as below.
 
-just some changes
-
-g
+#def hello_name(hello_USERNAME):
+hello_USERNAME = input("What is your name?: ")
+print ("HELLO! " + hello_USERNAME)
