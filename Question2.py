55ca120f0650eb76fcf9a28dc02e48f17fc65ed9
TobyM <tobymarkham@gmail.com>
2023-08-11 13:12:34 -0400

    1 & 2 finished?  am I uploading right? Still have google classroom.

--- Question2.py
+++ Question2.py
@@ -1,9 +1,13 @@
 #question 2
 #Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+#def first_odds():
 
-def first_odds(x):
+def first_odds():
     for x in range(1, 100, 2):
-        print(first_odds)
+        print(x)
+    else:
+        print("Mission Complete!")
+first_odds()
 
 
     
