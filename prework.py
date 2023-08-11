25533f2e54a40c45f6425c15c22ee851fa95193e
TobyM <tobymarkham@gmail.com>
2023-08-10 22:23:21 -0400

    Practicing the update

--- prework.py
+++ prework.py
@@ -1,7 +1,16 @@
 #Question 1
 #Write a function to print "hello_USERNAME!" is the input of the function.  The first 
 #line of the code has been defined as below.
-
 #def hello_name(hello_USERNAME):
+
 hello_USERNAME = input("What is your name?: ")
 print ("HELLO! " + hello_USERNAME)
+
+#question 2
+#Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+def first_odds():
+    x = 1
+    y = (x + 2)
+    print()
+    return
