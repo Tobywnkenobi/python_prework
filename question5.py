d912592b8b0b1df683abcab32e7faa7ab1e75cb1
TobyM <tobymarkham@gmail.com>
2023-08-12 15:30:06 -0400

    This one needs work

---
+++ question5.py
@@ -0,0 +1,23 @@
+#Write a function to check to see if all numbers in list are consecutive numbers.
+#For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers.
+#The return should be boolean Type
+#function to check the lists [use randome number lists, range??]
+#Check it, return true or false on it.
+
+
+list_a=[1,2,3,4,5,6,7,8,9,10,11,12]
+list_b=[1,2,5,7,8,4,0,84,3,6,345,65]
+
+
+def is_consecutive(list_a):
+    for i in list_a(0 , 12):
+            if int((1==(2-1))):True
+    else: False
+    if list_a is True:print(list_a)
+    is_consecutive(list_a)
+    
+
+    print(list_a)
+
+
+
